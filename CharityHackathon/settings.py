a1b26c4a1f725c3a07bec5f3f77b39d116b419da
heimu09 <twistr12345@gmail.com>
2023-04-08 00:35:32 +0600

    configurate heroku

--- CharityHackathon/settings.py
+++ CharityHackathon/settings.py
@@ -10,6 +10,7 @@ ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -102,6 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = 'static/'
 
 # Default primary key field type
