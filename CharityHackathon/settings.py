c1cf21e6f16e4554be6fb20487b009277c074964
heimu09 <twistr12345@gmail.com>
2023-04-08 11:37:18 +0600

    add new host

--- CharityHackathon/settings.py
+++ CharityHackathon/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-c5^zb%o*)6t18i1ry-px_4)=_9b@dfz=4ya31il4p2&(#lubsm
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["charity-hackathon.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["charity-hackathon.herokuapp.com", "127.0.0.1", 'localhost']
 
 
 INSTALLED_APPS = [
